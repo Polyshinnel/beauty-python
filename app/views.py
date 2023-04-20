d612aa094a6600e6b41bee8a163e69416a9a2e8a
polyshinnel <polyshinnel@gmail.com>
2023-04-20 15:35:08 +0300

    add authorize method

--- app/views.py
+++ app/views.py
@@ -6,9 +6,12 @@ import json
 
 @app.route('/')
 def index():
-    data = {"user2_proximity": 3, "Wifi_1": -80, "Wifi_2": -40, "Wifi_3": -40,
-            "thermostat": 18, "light": 0, "hour_of_day": 0, "user3_proximity": 3,
-            "user1_proximity": 1, "day_of_week": 1, "security": 0, "minute_of_hour": 9,
-            "Act_1": 1, "Act_2": 0, "Act_3": 0}
+    data = {'project-info': 'all is works'}
     json_data = json.dumps(data)
     return json_data
+
+
+@app.route('/authorize', methods=('POST',))
+def authorize():
+    data = request.form['username']
+    return data
